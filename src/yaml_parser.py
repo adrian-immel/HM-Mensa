2bb36f487c3bb6f5d746c3b91b9f5912863ea719
Adrian Immel <immel@hm.edu>
2025-04-23 16:32:24 +0200

    add html parser

---
+++ src/yaml_parser.py
@@ -0,0 +1,10 @@
+import yaml
+from yaml import SafeLoader
+
+with open('config.yml', 'r') as f:
+    data = yaml.full_load(f)
+
+# Print the values as a dictionary
+with open('multiple_documents.yml', 'r') as f:
+    yaml_data = list(yaml.load_all(f, Loader=SafeLoader))
+    print(yaml_data)
